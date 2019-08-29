aa6c3737c4ec95e4048e0342b80bf83bf3b6a797
Ruslan Akhtyamov <rahtyamov@getintent.com>
2019-08-29 05:16:26 +0300

    added code to get statistics about zip codes at file

---
+++ src/main/java/org/test/enricher/internal/StatsByZipCodes.java
@@ -0,0 +1,16 @@
+package org.test.enricher.internal;
+
+import org.test.enricher.datasource.TSVAddressesDataSource;
+import org.test.enricher.model.Address;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StatsByZipCodes {
+    public static void main(String[] args) throws Exception {
+        final var tsvAddressesDataSource = new TSVAddressesDataSource("result.tsv");
+        Map<String, Long> counts =
+                tsvAddressesDataSource.findAll().stream().map(Address::zipCode).collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        System.out.println(counts);
+    }
+}
